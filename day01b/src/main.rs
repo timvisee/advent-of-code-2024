879051996849d16659b425a0590044701e607967
timvisee <tim@visee.me>
2024-12-01 11:23:53 +0100

    Fix variable name typo

--- day01b/src/main.rs
+++ day01b/src/main.rs
@@ -1,13 +1,13 @@
 pub fn main() {
     let (mut a, mut b) = (Vec::with_capacity(1000), [0; 100_000]);
-    let num_lenl = include_bytes!("../input.txt")
+    let num_len = include_bytes!("../input.txt")
         .iter()
         .position(|&b| b == b' ')
         .unwrap();
 
     for line in include_bytes!("../input.txt").split(|&b| b == b'\n') {
-        a.push(atoi::atoi::<usize>(&line[0..num_lenl]).unwrap());
-        b[atoi::atoi::<usize>(&line[num_lenl + 3..]).unwrap()] += 1;
+        a.push(atoi::atoi::<usize>(&line[0..num_len]).unwrap());
+        b[atoi::atoi::<usize>(&line[num_len + 3..]).unwrap()] += 1;
     }
 
     println!("{}", a.into_iter().map(|n| b[n] * n).sum::<usize>());
