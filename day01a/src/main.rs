3f6297831ffca5d43117ebcbe842ac39c8b8b74d
timvisee <tim@visee.me>
2024-12-01 10:50:58 +0100

    Add day01a

--- day01a/src/main.rs
+++ day01a/src/main.rs
@@ -1,3 +1,23 @@
 pub fn main() {
-    println!("Hello, world!");
+    let mut a: Vec<usize> = Vec::new();
+    let mut b: Vec<usize> = Vec::new();
+
+    include_str!("../input.txt")
+        .lines()
+        .map(|line| line.split_once("   ").unwrap())
+        .for_each(|(left, right)| {
+            a.push(left.parse().unwrap());
+            b.push(right.parse().unwrap());
+        });
+
+    a.sort_unstable();
+    b.sort_unstable();
+
+    let sum = a
+        .into_iter()
+        .zip(b.into_iter())
+        .map(|(a, b)| a.abs_diff(b))
+        .sum::<usize>();
+
+    println!("{sum}");
 }
