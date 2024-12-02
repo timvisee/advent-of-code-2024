f6cf0564e07b4d5c527090860b54f738edcaeddf
timvisee <tim@visee.me>
2024-12-02 09:00:04 +0100

    Improve day02a

--- day02a/src/main.rs
+++ day02a/src/main.rs
@@ -1,21 +1,25 @@
+use itertools::Itertools;
+
 pub fn main() {
-    let count = include_bytes!("../input.txt")
-        .split(|&b| b == b'\n')
-        .filter(|line| {
-            let nums = line
-                .split(|&b| b == b' ')
-                .map(|n| atoi::atoi::<isize>(n).unwrap())
-                .collect::<Vec<_>>();
-
-            nums.windows(2).all(|n| {
-                let n = n[1].wrapping_sub(n[0]);
-                (1..=3).contains(&n)
-            }) || nums.windows(2).all(|n| {
-                let n = n[0].wrapping_sub(n[1]);
-                (1..=3).contains(&n)
+    println!(
+        "{}",
+        include_bytes!("../input.txt")
+            .split(|&b| b == b'\n')
+            .filter(|line| {
+                line.split(|&b| b == b' ')
+                    .map(|n| atoi::atoi::<isize>(n).unwrap())
+                    .tuple_windows()
+                    .try_fold(0, |acc, (a, b)| {
+                        if acc >= 0 && (1..=3).contains(&(b - a)) {
+                            Ok(acc + 1)
+                        } else if acc <= 0 && (1..=3).contains(&(a - b)) {
+                            Ok(acc - 1)
+                        } else {
+                            Err(())
+                        }
+                    })
+                    .is_ok()
             })
-        })
-        .count();
-
-    println!("{count}",);
+            .count(),
+    );
 }
